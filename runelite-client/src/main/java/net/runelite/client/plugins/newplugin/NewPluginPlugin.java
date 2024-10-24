f4f55b1fa54cd672930f4c784c36812097f2d9f0
Jesper van Berlo <berl0004@hz.nl>
2024-10-24 12:24:11 +0200

    add int variable

--- runelite-client/src/main/java/net/runelite/client/plugins/newplugin/NewPluginPlugin.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/newplugin/NewPluginPlugin.java
@@ -2,9 +2,5 @@ package net.runelite.client.plugins.newplugin;
 
 public class NewPluginPlugin {
 
-
-    // master branch test
-    // abort test master
-    // new plugin test 2
-
+    private int variable;
 }
